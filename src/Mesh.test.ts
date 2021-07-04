1f3e60a33172067e15738924ee839cd136eb88d2
Victor Widell <victor@topmost.se>
2021-07-04 15:08:48 +0200

    Refactor. Replaced the ids with direct references.

--- src/Mesh.test.ts
+++ src/Mesh.test.ts
@@ -7,25 +7,21 @@ export class MeshTestSuite {
 	@Test()
 	makeEmptyMesh() {
 		const mesh = makeEmptyMesh();
-		expect.toBeEqual(mesh.highestId, 0);
 		expect.toBeEqual(mesh.halfEdges.length, 0);
-		expect.toBeEqual(mesh.polygonIds.size, 0);
-		expect.toBeEqual(mesh.cornerIds.size, 0);
+		expect.toBeEqual(mesh.polygons.length, 0);
+		expect.toBeEqual(mesh.corners.length, 0);
 		expect.toBeEqual(mesh.cornerAttributes.size, 0);
-		expect.toBeEqual(mesh.polygons.size, 0);
 	}
 
 	@Test()
 	addCorner() {
 		const position = new Coord3({ x: 0, y: 0, z: 0 });
 		const normal = new Coord3({ x: 0, y: 0, z: 1 });
-		const { mesh, newCornerId } = addCorner(makeEmptyMesh(), {
+		const { mesh } = addCorner(makeEmptyMesh(), {
 			position,
 			normal,
 		});
-		expect.toBeGreater(newCornerId, 0);
-		expect.toBeEqual(mesh.highestId, newCornerId);
-		expect.toBeEqual(mesh.cornerIds.size, 1);
+		expect.toBeEqual(mesh.corners.length, 1);
 		expect.toBeEqual(mesh.cornerAttributes.size, 1);
 		expect.toBeEqual([...mesh.cornerAttributes.values()][0].position, position);
 	}
@@ -33,21 +29,21 @@ export class MeshTestSuite {
 	@Test()
 	extrudeCorner() {
 		const normal = new Coord3({ x: 0, y: 0, z: 1 });
-		let { mesh, newCornerId } = addCorner(makeEmptyMesh(), {
+		let { mesh, newCorner } = addCorner(makeEmptyMesh(), {
 			position: new Coord3({ x: -1, y: 0, z: 0 }),
 			normal,
 		});
-		mesh = extrudeCorner(mesh, newCornerId, new Coord3({ x: 2, y: 0, z: 0 }));
+		mesh = extrudeCorner(mesh, newCorner, new Coord3({ x: 2, y: 0, z: 0 }));
 		expect.toBeEqual(mesh.halfEdges.length, 2);
-		expect.toBeEqual(mesh.halfEdges[0].polygonId, undefined);
-		expect.toBeEqual(mesh.halfEdges[1].polygonId, undefined);
+		expect.toBeEqual(mesh.halfEdges[0].polygon, undefined);
+		expect.toBeEqual(mesh.halfEdges[1].polygon, undefined);
 		expect.toBeEqual(
-			mesh.halfEdges[0].nextEdgeIdAroundPolygon,
-			mesh.halfEdges[1].id,
+			mesh.halfEdges[0].nextEdgeAroundPolygon,
+			mesh.halfEdges[1],
 		);
 		expect.toBeEqual(
-			mesh.halfEdges[1].nextEdgeIdAroundPolygon,
-			mesh.halfEdges[0].id,
+			mesh.halfEdges[1].nextEdgeAroundPolygon,
+			mesh.halfEdges[0],
 		);
 	}
 }
