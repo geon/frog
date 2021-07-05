e12af7c72d953699482f4b89981d8c2d0ea4b314
Victor Widell <victor@topmost.se>
2021-07-05 18:39:32 +0200

    Added Mesh.

--- src/Mesh.test.ts
+++ src/Mesh.test.ts
@@ -1,72 +1,72 @@
 import { TestSuite, Test, expect } from "testyts";
-import { Coord3 } from "./Coord3";
-import { addCorner, extrudeCorner, makeEmptyMesh } from "./Mesh";
+import { makeEmptyMesh } from "./Mesh";
+import { getValues } from "./Table";
 
 @TestSuite()
 export class MeshTestSuite {
 	@Test()
 	makeEmptyMesh() {
 		const mesh = makeEmptyMesh();
-		expect.toBeEqual(mesh.halfEdges.length, 0);
-		expect.toBeEqual(mesh.polygons.length, 0);
-		expect.toBeEqual(mesh.corners.length, 0);
-		expect.toBeEqual(mesh.cornerAttributes.size, 0);
+		expect.toBeEqual(getValues(mesh.halfEdges).length, 0);
+		expect.toBeEqual(getValues(mesh.polygons).length, 0);
+		expect.toBeEqual(getValues(mesh.corners).length, 0);
+		expect.toBeEqual(getValues(mesh.cornerAttributes).length, 0);
 	}
 
-	@Test()
-	addCorner() {
-		const { mesh, position } = makeOneCornerMesh();
-		expect.toBeEqual(mesh.corners.length, 1);
-		expect.toBeEqual(mesh.cornerAttributes.size, 1);
-		expect.toBeEqual([...mesh.cornerAttributes.values()][0].position, position);
-	}
+	// @Test()
+	// addCorner() {
+	// 	const { mesh, position } = makeOneCornerMesh();
+	// 	expect.toBeEqual(mesh.corners.length, 1);
+	// 	expect.toBeEqual(mesh.cornerAttributes.size, 1);
+	// 	expect.toBeEqual([...mesh.cornerAttributes.values()][0].position, position);
+	// }
 
-	@Test()
-	extrudeCorner() {
-		let { mesh } = makeOneEdgeMesh();
-		expect.toBeEqual(mesh.halfEdges.length, 2);
-		expect.toBeEqual(mesh.halfEdges[0].polygon, undefined);
-		expect.toBeEqual(mesh.halfEdges[1].polygon, undefined);
-		expect.toBeEqual(
-			mesh.halfEdges[0].nextEdgeAroundPolygon,
-			mesh.halfEdges[1],
-		);
-		expect.toBeEqual(
-			mesh.halfEdges[1].nextEdgeAroundPolygon,
-			mesh.halfEdges[0],
-		);
-	}
+	// @Test()
+	// extrudeCorner() {
+	// 	let { mesh } = makeOneEdgeMesh();
+	// 	expect.toBeEqual(mesh.halfEdges.length, 2);
+	// 	expect.toBeEqual(mesh.halfEdges[0].polygon, undefined);
+	// 	expect.toBeEqual(mesh.halfEdges[1].polygon, undefined);
+	// 	expect.toBeEqual(
+	// 		mesh.halfEdges[0].nextEdgeAroundPolygon,
+	// 		mesh.halfEdges[1],
+	// 	);
+	// 	expect.toBeEqual(
+	// 		mesh.halfEdges[1].nextEdgeAroundPolygon,
+	// 		mesh.halfEdges[0],
+	// 	);
+	// }
 
-	@Test()
-	extrudeCornerOnEdge() {
-		const oneEdgeResult = makeOneEdgeMesh();
-		let { mesh, newHalfEdges } = extrudeCorner(
-			oneEdgeResult.mesh,
-			oneEdgeResult.mesh.corners[1],
-			new Coord3({ x: 1, y: 0, z: 0 }),
-		);
-		expect.toBeEqual(mesh.halfEdges.length, 4);
-		expect.toBeEqual(newHalfEdges[0].polygon, undefined);
-		expect.toBeEqual(newHalfEdges[1].polygon, undefined);
-		expect.toBeEqual(newHalfEdges[0].nextEdgeAroundPolygon, newHalfEdges[1]);
-		expect.toBeEqual(newHalfEdges[1].nextEdgeAroundPolygon, newHalfEdges[0]);
-	}
+	// @Test()
+	// extrudeCornerOnEdge() {
+	// 	const oneEdgeResult = makeOneEdgeMesh();
+	// 	let { mesh, newHalfEdges } = extrudeCorner(
+	// 		oneEdgeResult.mesh,
+	// 		oneEdgeResult.mesh.corners[1],
+	// 		new Coord3({ x: 1, y: 0, z: 0 }),
+	// 	);
+	// 	expect.toBeEqual(mesh.halfEdges.length, 4);
+	// 	expect.toBeEqual(newHalfEdges[0].polygon, undefined);
+	// 	expect.toBeEqual(newHalfEdges[1].polygon, undefined);
+	// 	expect.toBeEqual(newHalfEdges[0].nextEdgeAroundPolygon, newHalfEdges[1]);
+	// 	expect.toBeEqual(newHalfEdges[1].nextEdgeAroundPolygon, newHalfEdges[0]);
+	// }
 }
 
-function makeOneCornerMesh() {
-	const position = new Coord3({ x: 0, y: 0, z: 0 });
-	const normal = new Coord3({ x: 0, y: 0, z: 1 });
+// function makeOneCornerMesh() {
+// 	const position = new Coord3({ x: 0, y: 0, z: 0 });
+// 	const normal = new Coord3({ x: 0, y: 0, z: 1 });
 
-	return {
-		...addCorner(makeEmptyMesh(), {
-			position,
-			normal,
-		}),
-		position,
-	};
-}
+// 	return {
+// 		...addCorner(makeEmptyMesh(), {
+// 			position,
+// 			normal,
+// 		}),
+// 		position,
+// 	};
+// }
 
-function makeOneEdgeMesh() {
-	const { mesh, newCorner } = makeOneCornerMesh();
-	return extrudeCorner(mesh, newCorner, new Coord3({ x: 1, y: 0, z: 0 }));
-}
+// function makeOneEdgeMesh() {
+// 	const { mesh, newCorner } = makeOneCornerMesh();
+// 	return extrudeCorner(mesh, newCorner, new Coord3({ x: 1, y: 0, z: 0 }));
+// }
--- src/Mesh.ts
+++ src/Mesh.ts
@@ -1,183 +1,202 @@
 import { Coord3 } from "./Coord3";
-import * as twgl from "twgl.js";
+import { TwoDeepReadonly } from "./DeepReadonly";
+import { makeEmptyTable, MutableTable } from "./Table";
+// import * as twgl from "twgl.js";
 
-export interface Corner {}
+type CornerId = number;
+type EdgeId = number;
+type PolygonId = number;
+type HalfEdgeId = number;
+
+export interface Corner {
+	id: CornerId;
+	firstHalfEdgeId: HalfEdgeId;
+}
+
+export interface Edge {
+	id: EdgeId;
+	firstHalfEdgeId: HalfEdgeId;
+}
 
 export interface Polygon {
-	firstEdge: HalfEdge;
+	id: PolygonId;
+	firstEdgeId: HalfEdgeId;
+	isHole: boolean;
 }
 
 export interface HalfEdge {
-	corner: Corner;
-	otherHalf: HalfEdge;
-	polygon?: Polygon;
+	id: HalfEdgeId;
+	cornerId: CornerId;
+	otherHalfId: HalfEdgeId;
+	polygonId: PolygonId;
 
 	// Next edge along the Polygon perimeter clock-wise.
-	nextEdgeAroundPolygon: HalfEdge;
+	nextEdgeIdAroundPolygon: HalfEdgeId;
 	// Next edge around the corner anti-clock-wise.
-	// nextEdgeAroundCorner: HalfEdge;
+	nextEdgeIdAroundCorner: HalfEdgeId;
 }
 
-export interface Mesh {
-	readonly halfEdges: readonly HalfEdge[];
-	readonly corners: readonly Corner[];
-	readonly polygons: readonly Polygon[];
-	readonly cornerAttributes: ReadonlyMap<
-		Corner,
-		{
-			position: Coord3;
-			normal: Coord3;
-		}
-	>;
+export interface MutableMesh {
+	corners: MutableTable<Corner>;
+	edges: MutableTable<Edge>;
+	polygons: MutableTable<Polygon>;
+	halfEdges: MutableTable<HalfEdge>;
+	cornerAttributes: MutableTable<{
+		position: Coord3;
+		normal: Coord3;
+	}>;
 }
+
+export type Mesh = TwoDeepReadonly<MutableMesh>;
 
 export function makeEmptyMesh(): Mesh {
 	return {
-		halfEdges: [],
-		corners: [],
-		polygons: [],
-		cornerAttributes: new Map(),
+		corners: makeEmptyTable(),
+		edges: makeEmptyTable(),
+		polygons: makeEmptyTable(),
+		halfEdges: makeEmptyTable(),
+		cornerAttributes: makeEmptyTable(),
 	};
 }
 
-export function addCorner(
-	mesh: Mesh,
-	attributes: { position: Coord3; normal: Coord3 },
-): { mesh: Mesh; newCorner: Corner } {
-	const newCorner: Corner = {};
+// export function addCorner(
+// 	mesh: Mesh,
+// 	attributes: { position: Coord3; normal: Coord3 },
+// ): { mesh: Mesh; newCorner: Corner } {
+// 	const newCorner: Corner = {};
 
-	return {
-		mesh: {
-			...mesh,
-			corners: [...mesh.corners, newCorner],
-			cornerAttributes: new Map([
-				...mesh.cornerAttributes,
-				[newCorner, attributes],
-			]),
-		},
-		newCorner,
-	};
-}
+// 	return {
+// 		mesh: {
+// 			...mesh,
+// 			corners: [...mesh.corners, newCorner],
+// 			cornerAttributes: new Map([
+// 				...mesh.cornerAttributes,
+// 				[newCorner, attributes],
+// 			]),
+// 		},
+// 		newCorner,
+// 	};
+// }
 
-export function extrudeCorner(
-	mesh: Mesh,
-	corner: Corner,
-	offset: Coord3,
-): { mesh: Mesh; newHalfEdges: HalfEdge[] } {
-	const oldCornerAttributes = mesh.cornerAttributes.get(corner)!;
+// export function extrudeCorner(
+// 	mesh: Mesh,
+// 	corner: Corner,
+// 	offset: Coord3,
+// ): { mesh: Mesh; newHalfEdges: HalfEdge[] } {
+// 	const oldCornerAttributes = mesh.cornerAttributes.get(corner)!;
 
-	const { newCorner, ...rest } = addCorner(mesh, {
-		...oldCornerAttributes,
-		position: Coord3.add(oldCornerAttributes.position, offset),
-	});
-	mesh = rest.mesh;
+// 	const { newCorner, ...rest } = addCorner(mesh, {
+// 		...oldCornerAttributes,
+// 		position: Coord3.add(oldCornerAttributes.position, offset),
+// 	});
+// 	mesh = rest.mesh;
 
-	const halfEdgeA: HalfEdge = {
-		corner: corner,
-		// The othe edge does not exist yet.
-		otherHalf: undefined!,
-		nextEdgeAroundPolygon: undefined!,
-	};
-	const halfEdgeB: HalfEdge = {
-		corner: newCorner,
-		otherHalf: halfEdgeA!,
-		nextEdgeAroundPolygon: halfEdgeA,
-	};
-	// Now it exists.
-	halfEdgeA.otherHalf = halfEdgeB;
-	halfEdgeA.nextEdgeAroundPolygon = halfEdgeB;
+// 	const halfEdgeA: HalfEdge = {
+// 		corner: corner,
+// 		// The othe edge does not exist yet.
+// 		otherHalf: undefined!,
+// 		nextEdgeAroundPolygon: undefined!,
+// 	};
+// 	const halfEdgeB: HalfEdge = {
+// 		corner: newCorner,
+// 		otherHalf: halfEdgeA!,
+// 		nextEdgeAroundPolygon: halfEdgeA,
+// 	};
+// 	// Now it exists.
+// 	halfEdgeA.otherHalf = halfEdgeB;
+// 	halfEdgeA.nextEdgeAroundPolygon = halfEdgeB;
 
-	const newHalfEdges = [halfEdgeA, halfEdgeB];
+// 	const newHalfEdges = [halfEdgeA, halfEdgeB];
 
-	return {
-		mesh: { ...mesh, halfEdges: [...mesh.halfEdges, ...newHalfEdges] },
-		newHalfEdges,
-	};
-}
+// 	return {
+// 		mesh: { ...mesh, halfEdges: [...mesh.halfEdges, ...newHalfEdges] },
+// 		newHalfEdges,
+// 	};
+// }
 
-export function makeTestMesh(): Mesh {
-	return makeEmptyMesh();
-}
+// export function makeTestMesh(): Mesh {
+// 	return makeEmptyMesh();
+// }
 
-function coord3ToTwglVec3(coord: Coord3): twgl.v3.Vec3 {
-	return [coord.x, coord.y, coord.z];
-}
+// function coord3ToTwglVec3(coord: Coord3): twgl.v3.Vec3 {
+// 	return [coord.x, coord.y, coord.z];
+// }
 
-export function meshToWebglArrays(mesh: Mesh): twgl.Arrays {
-	const polygonArrays = mesh.polygons.map((polygon) =>
-		polygonToWebglArrays(mesh, polygon),
-	);
+// export function meshToWebglArrays(mesh: Mesh): twgl.Arrays {
+// 	const polygonArrays = mesh.polygons.map((polygon) =>
+// 		polygonToWebglArrays(mesh, polygon),
+// 	);
 
-	let beginIndex = 0;
-	const data: {
-		position: Coord3[];
-		normal: Coord3[];
-		indices: number[];
-	} = {
-		position: [],
-		normal: [],
-		indices: [],
-	};
-	for (const current of polygonArrays) {
-		data.position.push(...current.position);
-		data.normal.push(...current.normal);
-		data.indices.push(...current.indices.map((index) => index + beginIndex));
-		beginIndex += current.position.length;
-	}
+// 	let beginIndex = 0;
+// 	const data: {
+// 		position: Coord3[];
+// 		normal: Coord3[];
+// 		indices: number[];
+// 	} = {
+// 		position: [],
+// 		normal: [],
+// 		indices: [],
+// 	};
+// 	for (const current of polygonArrays) {
+// 		data.position.push(...current.position);
+// 		data.normal.push(...current.normal);
+// 		data.indices.push(...current.indices.map((index) => index + beginIndex));
+// 		beginIndex += current.position.length;
+// 	}
 
-	return {
-		position: {
-			numComponents: 3,
-			data: data.position
-				.map(coord3ToTwglVec3)
-				.reduce<number[]>((soFar, current) => {
-					soFar.push(...current);
-					return soFar;
-				}, []),
-		},
-		normal: {
-			numComponents: 3,
-			data: data.normal
-				.map(coord3ToTwglVec3)
-				.reduce<number[]>((soFar, current) => {
-					soFar.push(...current);
-					return soFar;
-				}, []),
-		},
-		indices: {
-			numComponents: 3,
-			data: data.indices,
-		},
-	};
-}
+// 	return {
+// 		position: {
+// 			numComponents: 3,
+// 			data: data.position
+// 				.map(coord3ToTwglVec3)
+// 				.reduce<number[]>((soFar, current) => {
+// 					soFar.push(...current);
+// 					return soFar;
+// 				}, []),
+// 		},
+// 		normal: {
+// 			numComponents: 3,
+// 			data: data.normal
+// 				.map(coord3ToTwglVec3)
+// 				.reduce<number[]>((soFar, current) => {
+// 					soFar.push(...current);
+// 					return soFar;
+// 				}, []),
+// 		},
+// 		indices: {
+// 			numComponents: 3,
+// 			data: data.indices,
+// 		},
+// 	};
+// }
 
-function* polygonCorners(polygon: Polygon) {
-	let halfEdge = polygon.firstEdge;
-	do {
-		yield halfEdge.corner;
-		halfEdge = halfEdge.nextEdgeAroundPolygon;
-	} while (halfEdge !== polygon.firstEdge);
-}
+// function* polygonCorners(polygon: Polygon) {
+// 	let halfEdge = polygon.firstEdge;
+// 	do {
+// 		yield halfEdge.corner;
+// 		halfEdge = halfEdge.nextEdgeAroundPolygon;
+// 	} while (halfEdge !== polygon.firstEdge);
+// }
 
-function polygonToWebglArrays(mesh: Mesh, polygon: Polygon) {
-	const corners = [...polygonCorners(polygon)];
+// function polygonToWebglArrays(mesh: Mesh, polygon: Polygon) {
+// 	const corners = [...polygonCorners(polygon)];
 
-	const vertices = corners.map((corner) => mesh.cornerAttributes.get(corner)!);
+// 	const vertices = corners.map((corner) => mesh.cornerAttributes.get(corner)!);
 
-	const cornerIndices = corners.map((corner) =>
-		vertices.indexOf(mesh.cornerAttributes.get(corner)!),
-	);
+// 	const cornerIndices = corners.map((corner) =>
+// 		vertices.indexOf(mesh.cornerAttributes.get(corner)!),
+// 	);
 
-	return {
-		position: vertices.map((vertex) => vertex.position),
-		normal: vertices.map((vertex) => vertex.normal),
-		indices:
-			cornerIndices.length === 3
-				? cornerIndices
-				: [
-						...cornerIndices.slice(0, 3),
-						...cornerIndices.slice(2, 4),
-						cornerIndices[0],
-				  ],
-	};
-}
+// 	return {
+// 		position: vertices.map((vertex) => vertex.position),
+// 		normal: vertices.map((vertex) => vertex.normal),
+// 		indices:
+// 			cornerIndices.length === 3
+// 				? cornerIndices
+// 				: [
+// 						...cornerIndices.slice(0, 3),
+// 						...cornerIndices.slice(2, 4),
+// 						cornerIndices[0],
+// 				  ],
+// 	};
+// }
