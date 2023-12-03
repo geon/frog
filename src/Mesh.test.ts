2416e6358c75ea64073f6059f0de347b7b3ae14a
Victor Widell <victor.widell@gmail.com>
2023-12-03 15:22:34 +0100

    Added test: cornerEdgeCount 1

--- src/Mesh.test.ts
+++ src/Mesh.test.ts
@@ -1,6 +1,22 @@
-import { test } from "vitest";
-import { Mesh } from "./Mesh";
+import { expect, test } from "vitest";
+import { Corner, HalfEdge, Mesh } from "./Mesh";
+import { Coord3 } from "./Coord3";
 
 test("new Mesh", () => {
 	new Mesh();
+});
+
+test("cornerEdgeCount 1", () => {
+	const corner: Corner = {
+		position: new Coord3({ x: 0, y: 0, z: 0 }),
+		firstHalfEdge: null!,
+	};
+	const halfEdge: HalfEdge = {
+		next: null!,
+	};
+
+	corner.firstHalfEdge = halfEdge;
+	halfEdge.next = halfEdge;
+
+	expect(Mesh.cornerEdgeCount(corner)).toBe(1);
 });
--- src/Mesh.ts
+++ src/Mesh.ts
@@ -1,3 +1,25 @@
+import { Coord3 } from "./Coord3";
+
+export interface Corner {
+	firstHalfEdge: HalfEdge;
+	position: Coord3;
+}
+
+export interface HalfEdge {
+	// Next half-edge around the corner clockwise.
+	next: HalfEdge;
+}
+
 export class Mesh {
 	constructor() {}
+
+	static cornerEdgeCount(corner: Corner): number {
+		let count = 0;
+		let halfEdge = corner.firstHalfEdge;
+		do {
+			halfEdge = halfEdge.next;
+			++count;
+		} while (halfEdge !== corner.firstHalfEdge);
+		return count;
+	}
 }
