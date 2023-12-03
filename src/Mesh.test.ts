f7bf9dd18cea2cf02f529bced5fd0c2224acaf6c
Victor Widell <victor.widell@gmail.com>
2023-12-03 15:24:21 +0100

    Added test: cornerEdgeCount 2

--- src/Mesh.test.ts
+++ src/Mesh.test.ts
@@ -20,3 +20,22 @@ test("cornerEdgeCount 1", () => {
 
 	expect(Mesh.cornerEdgeCount(corner)).toBe(1);
 });
+
+test("cornerEdgeCount 2", () => {
+	const corner: Corner = {
+		position: new Coord3({ x: 0, y: 0, z: 0 }),
+		firstHalfEdge: null!,
+	};
+	const halfEdgeA: HalfEdge = {
+		next: null!,
+	};
+	const halfEdgeB: HalfEdge = {
+		next: null!,
+	};
+
+	corner.firstHalfEdge = halfEdgeA;
+	halfEdgeA.next = halfEdgeB;
+	halfEdgeB.next = halfEdgeA;
+
+	expect(Mesh.cornerEdgeCount(corner)).toBe(2);
+});
